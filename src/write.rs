2b4fc02764b12eac08d4429abf13557a87e9b387
Logan SQUIREL <logan.squirel@protonmail.com>
2020-11-15 17:31:45 +0100

    Add Write trait implementations

--- src/write.rs
+++ src/write.rs
@@ -1,5 +1,50 @@
 use crate::Error;
 
 pub trait Write {
-    fn write(&mut self, byte: &[u8]) -> Result<(), Error>;
+    fn write(&mut self, bytes: &[u8]) -> Result<(), Error>;
+}
+
+pub struct IoWrite<W> {
+    writer: W,
+}
+
+impl<W> IoWrite<W>
+where
+    W: std::io::Write,
+{
+    pub fn new(writer: W) -> Self {
+        IoWrite { writer }
+    }
+}
+
+impl<W> Write for IoWrite<W>
+where
+    W: std::io::Write,
+{
+    fn write(&mut self, bytes: &[u8]) -> Result<(), Error> {
+        self.writer.write_all(bytes).map_err(|_| Error)
+    }
+}
+
+pub struct FmtWrite<W> {
+    writer: W,
+}
+
+impl<W> FmtWrite<W>
+where
+    W: std::fmt::Write,
+{
+    pub fn new(writer: W) -> Self {
+        FmtWrite { writer }
+    }
+}
+
+impl<W> Write for FmtWrite<W>
+where
+    W: std::fmt::Write,
+{
+    fn write(&mut self, bytes: &[u8]) -> Result<(), Error> {
+        let string = unsafe { std::str::from_utf8_unchecked(bytes) };
+        self.writer.write_str(string).map_err(|_| Error)
+    }
 }
