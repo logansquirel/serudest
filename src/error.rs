ca995f0c8872333b178fcbbd01c6a9323c4e2418
Logan SQUIREL <logan.squirel@protonmail.com>
2020-11-15 10:44:04 +0100

    Add error::Error

--- src/error.rs
+++ src/error.rs
@@ -1,1 +1,10 @@
+#[derive(Debug)]
+pub struct Error;
 
+impl std::error::Error for Error {}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        formatter.write_str("error")
+    }
+}
